778e5bb027bb8565a962866c65f067e9b8a7829c
DStark <hindash@email.arizona.edu>
2020-01-14 11:25:42 -0700

    routing updates

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,34 +1,15 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+
 var path = require("path");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function (app)
 {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/tables", function (req, res)
+  app.get("/", function (err, res)
   {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
-  app.get("/reserve", function (req, res)
+  app.get("/survey", function (err, res)
   {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"))
   });
-
-  // If no matching route is found default to home
-  app.get("*", function (req, res)
-  {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-};
+}
