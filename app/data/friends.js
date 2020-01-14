ebc7c5742cdacc6e7c8e34c7e4c955e863530ce4
DStark <hindash@email.arizona.edu>
2020-01-14 10:53:31 -0700

    functionality additions

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+// (DATA)
+// ===========================================
+var friends = [
+  {
+    name: "Yoda",
+    age: 900,
+    score: 2000
+  },
+  {
+    name: "Darth Maul",
+    age: 200,
+    score: 1200
+  },
+  {
+    name: "Obi Wan Kenobi",
+    age: 55,
+    score: 1350
+  }
+];
